ade104994be370cd2c1b688fde7d61ad50da66cd
justinmajetich <justinmajetich@gmail.com>
2019-12-10 19:36:16 +0000

    Change add else to minimize print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{:02d}".format(i), end='')
     if i < 99:
-        print(", ", end='')
-print("")
+        print("{:02d}, ".format(i), end='')
+    else:
+        print("{:02d}".format(i))
