3b80aec0e2853254c45f32b3b882c2d9f9ce15db
justinmajetich <justinmajetich@gmail.com>
2019-12-10 22:57:08 +0000

    Check if number is equal

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,37 +11,33 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *curr = NULL, *new = NULL;
 
-	if (head)
+	curr = *head;
+	if (!(*head) || curr->n >= number)
 	{
-		curr = *head;
-		if (!(*head) || curr->n > number)
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+
+		new->n = number;
+		new->next = curr;
+		*head = new;
+		return (new);
+	}
+	while (curr)
+	{
+		if (curr->n <= number && (!curr->next || curr->next->n > number))
 		{
 			new = malloc(sizeof(listint_t));
 			if (!new)
 				return (NULL);
 
 			new->n = number;
-			new->next = curr;
-			*head = new;
+			new->next = curr->next;
+			curr->next = new;
 			return (new);
 		}
-		while (curr)
-		{
-			if (curr->n < number && (!curr->next || curr->next->n > number))
-			{
-				new = malloc(sizeof(listint_t));
-				if (!new)
-					return (NULL);
-
-				new->n = number;
-				new->next = curr->next;
-				curr->next = new;
-				return (new);
-			}
-			else
-				curr = curr->next;
-		}
-		return (new);
+		else
+			curr = curr->next;
 	}
 	return (NULL);
 }
