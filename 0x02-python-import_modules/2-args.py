e873e8260c8c2601dab0754f528d8e2684a2091d
justinmajetich <justinmajetich@gmail.com>
2019-12-12 18:04:53 +0000

    Add specific print statement if only one arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     from sys import argv
 
 argc = len(argv) - 1
-if argc > 0:
+if argc > 1:
     print("{:d} arguments:".format(argc))
+elif argc == 1:
+    print("1 argument:")
 else:
     print("0 arguments.")
 
