e118722f7ea6e66be696145557c53939b8bde8db
justinmajetich <justinmajetich@gmail.com>
2019-12-12 19:29:38 +0000

    Print requested formatting

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,14 +7,18 @@ if len(argv) is not 4:
     print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
     exit(1)
 
-if argv[2] is '+':
-    print("{}".format(add(int(argv[1]), int(argv[3]))))
-elif argv[2] is '-':
-    print("{}".format(sub(int(argv[1]), int(argv[3]))))
-elif argv[2] is '*':
-    print("{}".format(mul(int(argv[1]), int(argv[3]))))
-elif argv[2] is '/':
-    print("{}".format(div(int(argv[1]), int(argv[3]))))
+a = argv[1]
+b = argv[3]
+op = argv[2]
+
+if op is '+':
+    print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+elif op is '-':
+    print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
+elif op is '*':
+    print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
+elif op is '/':
+    print("{} / {} = {}".format(a, b, div(int(a), int(b))))
 else:
     print("{}".format("Unknown operator. Available operators: +, -, * and /"))
     exit(1)
