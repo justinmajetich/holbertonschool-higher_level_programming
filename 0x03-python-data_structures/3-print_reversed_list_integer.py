8a9f7a0e14ca6a138c4a04b26023db5916abff32
justinmajetich <justinmajetich@gmail.com>
2019-12-17 20:16:04 +0000

    Filter out empty lists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    #  [start:stop:increment]
-    for i in my_list[::-1]:
-        print("{:d}".format(i))
+    if len(my_list) > 0:
+        #  [start:stop:increment]
+        for i in my_list[::-1]:
+            print("{:d}".format(i))
