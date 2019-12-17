ff6e024ecf444d087f22e35bc2e47294ef67b0a0
justinmajetich <justinmajetich@gmail.com>
2019-12-17 18:36:27 +0000

    Remove iteration start parameter

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     #  [start:stop:increment]
-    for i in my_list[-1::-1]:
+    for i in my_list[::-1]:
         print("{:d}".format(i))
