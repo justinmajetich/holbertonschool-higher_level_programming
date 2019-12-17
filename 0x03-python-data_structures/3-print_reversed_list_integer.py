9195a0a1d4606523548a5d437668783129bab7a4
justinmajetich <justinmajetich@gmail.com>
2019-12-17 20:20:46 +0000

    Change conditional guard

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) > 0:
+    if my_list:
         #  [start:stop:increment]
         for i in my_list[::-1]:
             print("{:d}".format(i))
