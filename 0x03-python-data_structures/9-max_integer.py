7b8da04c52d9529e0fb7fd83269061f4ff57fa07
justinmajetich <justinmajetich@gmail.com>
2019-12-17 22:20:16 +0000

    Use loop instead of max() built-in

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        return max(my_list)
+        x = 0
+        for i in my_list:
+            if i > x:
+                x = i
+        return x
     else:
         return None
