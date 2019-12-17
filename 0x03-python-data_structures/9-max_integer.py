9bced36e2bf2f7acf4f82a0a7af8f1100c8723be
justinmajetich <justinmajetich@gmail.com>
2019-12-17 22:36:43 +0000

    Treat max of 0 and empty list differently

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    x = 0
-    for i in my_list:
-        if i > x:
-            x = i
-    if x is 0:
-        x = None
-    return x
+    if my_list:
+        x = 0
+        for i in my_list:
+            if i > x:
+                x = i
+        return x
+    else:
+        return None
