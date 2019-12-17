f63437405c13df57bcd487d3826dc076e096b4d5
justinmajetich <justinmajetich@gmail.com>
2019-12-17 21:19:20 +0000

    Add format specifiers to int print statements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             if col is row[-1]:
-                print("{}".format(col), end="")
+                print("{:d}".format(col), end="")
             else:
-                print("{}".format(col), end=" ")
+                print("{:d}".format(col), end=" ")
         print("")
