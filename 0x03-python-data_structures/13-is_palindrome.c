a197113f28a3c03d18d423e0f7967a0d21a660ad
justinmajetich <justinmajetich@gmail.com>
2019-12-18 03:36:40 +0000

    Re-write algorithm to work without list copy

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-listint_t *add_nodeint(listint_t **head, const int n);
-int check_cycle(listint_t *list);
 /**
  * is_palindrome - check if a singly-linked list palindromic
  * @head: list head
@@ -9,105 +7,31 @@ int check_cycle(listint_t *list);
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *rev = NULL, *temp = NULL, *rtemp = NULL;
-	size_t list_len = 0, center;
+	listint_t *left = NULL, *right = NULL;
+	size_t len = 0, i, loff, roff; /* length and offsets */
 
 	if (!head || !(*head))
 		return (1);
 
-/*	if (check_cycle(*head)) if list has cycle
-		return (0);*/
-
-	/* create list in reverse to compare against */
-	temp = *head;
-	while (temp)
+	/* send right pointer to end of list */
+	left = right = *head;
+	while (right->next)
 	{
-		add_nodeint(&rev, temp->n);
-		temp = temp->next;
-		list_len++;
+		right = right->next;
+		len++;
 	}
-	/* compare list to reverse copy */
-	if (list_len % 2 == 0)
-		center = list_len / 2;
-	else
-		center = (list_len / 2) + 1;
-
-	temp = *head;
-	rtemp = rev;
-	while (center--)
+	if (left->n == right->n)
 	{
-		if (temp->n != rtemp->n)
+		for (roff = loff = 1; len > (loff + roff); loff++, roff++)
 		{
-			free_listint(rev);
-			return (0);
+			left = left->next;
+			for (right = left, i = 0; i < (len - (loff + roff)); i++)
+				right = right->next;
+			if (left->n != right->n) /* if values don't match */
+				return (0);
 		}
-		temp = temp->next;
-		rtemp = rtemp->next;
 	}
-	free_listint(rev);
-	return (1);
-}
-/**
- * add_nodeint - add new node to beginning of list
- * @head: pointer to list head
- * @n: value to initialize in new node
- *
- * Return: address of new node on Success, NULL on Fail
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t node; /* new node */
-	listint_t *new = &node; /* pointer to new node */
-
-	/* check for NULL head */
-	if (!head)
-		return (NULL);
-
-	/* allocate for new node */
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
-
-	/* initialize new node */
-	new->n = n;
-	new->next = *head; /* point new head to old head */
-
-	/* assign head pointer to new head */
-	*head = new;
-
-	return (new);
-}
-/**
- * check_cycle - check for cycle in linked list
- * @list: list to check
- *
- * Return: 1 if cycle detected, 0 if no cycle
- */
-int check_cycle(listint_t *list)
-{
-	listint_t *fast; /* pointer to traverse list */
-
-	if (list && list->next)
-		fast = list->next;
 	else
 		return (0);
-
-	while (list && fast) /* traverse list till NULL */
-	{
-		if (list == fast) /* if pointers meet */
-			return (1); /* cycle detected */
-
-		list = list->next; /* traverse list at diff speeds */
-
-		if (fast->next)
-			fast = fast->next;
-		else
-			return (0);
-
-		if (fast->next)
-			fast = fast->next;
-		else
-			return (0);
-	}
-	return (0); /* no cycle detected */
+	return (1);
 }
