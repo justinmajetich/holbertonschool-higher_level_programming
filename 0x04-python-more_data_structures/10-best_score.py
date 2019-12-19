5c902972608e9257e398e73871cb2782f36bc365
justinmajetich <justinmajetich@gmail.com>
2019-12-19 20:56:29 +0000

    Use max() method

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        for k, v in reversed(sorted(a_dictionary.items())):
-            return k
+        for k, v in a_dictionary.items():
+            if v is max(a_dictionary.values()):
+                return k
     else:
         return None
