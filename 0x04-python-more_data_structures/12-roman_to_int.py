66a4ff11f08387105a2b38d39b85625c632eeaf5
justinmajetich <justinmajetich@gmail.com>
2019-12-20 01:01:35 +0000

    Check if arg passed is str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     number = 0
     skip = 0
+    if roman_string is None or type(roman_string) is not str:
+        return 0
     for i, letter in enumerate(roman_string):  # iterate through string
         if skip is 0:
             if letter in d:  # if letter exists in dict
