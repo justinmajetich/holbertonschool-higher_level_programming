f7f190d79cf33cab9ac37b914b08f61aadf83f5c
justinmajetich <justinmajetich@gmail.com>
2019-12-20 00:55:46 +0000

    Write script to convert roman numeral to decimal

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    skip = 0
+    for i, letter in enumerate(roman_string):  # iterate through string
+        if skip is 0:
+            if letter in d:  # if letter exists in dict
+                if i < (len(roman_string) - 1):  # letter is not last in string
+                    if d[letter] < d[roman_string[i + 1]]:
+                        number += (d[roman_string[i + 1]] - d[letter])
+                        skip = 1
+                        continue
+                    else:
+                        number += d[letter]
+                else:  # if letter is last in string
+                    number += d[letter]
+            else:
+                return 0
+            skip = 0
+    return number
