3bae4503e4183481f5159f0a226c96d9018c6323
justinmajetich <justinmajetich@gmail.com>
2020-01-08 17:56:00 +0000

    Remove semicolon

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             cnt += 1
         except IndexError:
-            break;
+            break
     print('')
     return cnt
