ce10f680bef913bf5e6f1d652e760f74bf0221ba
justinmajetich <justinmajetich@gmail.com>
2020-01-08 17:54:50 +0000

    Use a counter instead of range index to track prints

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    cnt = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
+            cnt += 1
         except IndexError:
-            i -= 1
             break;
     print('')
-    return i + 1
+    return cnt
