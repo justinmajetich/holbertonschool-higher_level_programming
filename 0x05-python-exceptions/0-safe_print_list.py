ff410ea428bd1d832ab74a424d0391956190b806
justinmajetich <justinmajetich@gmail.com>
2020-01-08 17:51:47 +0000

    Add break statement to except case

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i, elem in enumerate(my_list):
+    for i in range(x):
         try:
-            if i < x:
-                print(elem, end='')
-            else:
-                print('')
-                return i
+            print(my_list[i], end='')
         except IndexError:
-            None
+            i -= 1
+            break;
     print('')
     return i + 1
