f3e3d704d647d322ddca8514bb4d2af8e83127ef
justinmajetich <justinmajetich@gmail.com>
2020-01-09 21:04:23 +0000

    Create a class with method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a class Square with attributes"""
+
+
+class Square:
+    """Defines a class square"""
+    def __init__(self, size=0):
+        """Instantiates class object with size attribute
+
+        Args:
+            size: size of square
+
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is < 0
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Takes area of a square
+
+        Returns:
+            The area of a square
+        """
+        return self.__size ** 2
