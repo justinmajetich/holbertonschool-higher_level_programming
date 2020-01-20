cde65d95b9515a773dc4d89dc0b3a9179a00126e
justinmajetich <justinmajetich@gmail.com>
2020-01-20 19:24:14 +0000

    Write rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module defines a class, Rectangle"""
+
+
+class BaseGeometry:
+    """A class defining a geometric shape"""
+
+    def integer_validator(self, name, value):
+        """Validates name/value argument
+        Arguments:
+            name: name associated with value
+            value: value (must be integer > 0)
+        """
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+    def area(self):
+        """Take area of shape"""
+        raise Exception('area() is not implemented')
+
+
+class Rectangle(BaseGeometry):
+    """A class definig a rectangle """
+    def __init__(self, width, height):
+        """Instantiates class object
+
+        Arguments:
+            width: width of rectangle
+            height: height of rectangle
+        """
+        self.__width = self.integer_validator('width', width)
+        self.__height = self.integer_validator('height', height)
