b1d234d4ed385ce82e1ae0656afc2cb9bb2c336c
justinmajetich <justinmajetich@gmail.com>
2020-01-20 19:41:18 +0000

    Fix validator block

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -30,5 +30,7 @@ class Rectangle(BaseGeometry):
             width: width of rectangle
             height: height of rectangle
         """
-        self.__width = self.integer_validator('width', width)
-        self.__height = self.integer_validator('height', height)
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
