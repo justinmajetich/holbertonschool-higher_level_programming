c639c502f84477f9c8f11e66c0a61098c1f6754d
justinmajetich <justinmajetich@gmail.com>
2020-01-21 19:58:19 +0000

    write function to count lines in file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""This module defines a function to count lines read from file"""
+
+
+def number_of_lines(filename=""):
+    """Return number of lines read"""
+    line_count = 0
+    with open(filename, mode='r', encoding='UTF-8') as f:
+        for line in f:
+            line_count += 1
+    return line_count
