a7e9e2a30184222f843af2d4ba1e4dfccb21da7f
justinmajetich <justinmajetich@gmail.com>
2020-01-21 20:24:56 +0000

    Write function to read n lines from file

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a function the reads and prints n lines from file"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Read and print n lines from file
+    Arguments:
+        filename: file to read from
+        nb_lines: number of lines to read/print
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        for line_count, lines in enumerate(f):
+            pass
+        if nb_lines <= 0 or nb_lines > (line_count + 1):
+            f.seek(0)
+            print(f.read(), end='')
+        else:
+            f.seek(0)  # return to file beginning
+            for line in range(nb_lines):
+                print(f.readline(), end='')
