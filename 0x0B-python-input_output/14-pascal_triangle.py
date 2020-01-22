1d6a411ed724e3c29c0d209d2ba66aa98508d9a8
justinmajetich <justinmajetich@gmail.com>
2020-01-22 04:38:42 +0000

    Create module

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a function that creates a Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """Return a Pascal's triangle
+    """
+    if n <= 0:
+        return []
