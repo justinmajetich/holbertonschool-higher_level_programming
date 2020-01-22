53bf55f7a726a86828de202a6007f0273f0aa499
justinmajetich <justinmajetich@gmail.com>
2020-01-22 17:24:50 +0000

    Write program to parse logs

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines a program that parses stdin
+"""
+
+
+import sys
+
+codes = ['200', '301', '400', '401',
+         '403', '404', '405', '500']
+code_hist = []
+total_size = 0
+
+try:
+    while True:
+        for cnt, line in enumerate(sys.stdin):
+            tokens = line.split(' ')
+            total_size += int(tokens[-1])
+            code_hist.append(tokens[-2])
+            if cnt == 9:
+                print('File size: ' + str(total_size))
+                for code in codes:
+                    if code in code_hist:
+                        print('{}: {}'.format(code, code_hist.count(code)))
+                break
+except KeyboardInterrupt:
+    print('File size: ' + str(total_size))
+    for code in codes:
+        if code in code_hist:
+            print('{}: {}'.format(code, code_hist.count(code)))
+    sys.exit()
