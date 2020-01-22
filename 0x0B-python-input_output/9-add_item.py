f83e2debe9e9be3abd8153c2371593554c417615
justinmajetich <justinmajetich@gmail.com>
2020-01-22 02:11:22 +0000

    Write program to add args to a JSON object

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,14 +2,19 @@
 """This module defines a function which compiles all args into file"""
 
 
+import sys
+import os
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-import sys
 args = sys.argv
 
-obj = load_from_json_file(args[0])
+if not os.path.exists('add_item.json'):
+    with open('add_item.json', mode='w', encoding='utf-8') as f:
+        f.write('[]')
+
+obj = load_from_json_file('add_item.json')
 
 for arg in args[1:]:
     obj.append(arg)
 
-save_to_json_file(f, 'add_item.json')
+save_to_json_file(obj, 'add_item.json')
