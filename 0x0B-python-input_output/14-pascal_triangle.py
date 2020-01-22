4678162d19fcef90e31de84f9078091e176ac73a
justinmajetich <justinmajetich@gmail.com>
2020-01-22 14:40:28 +0000

    Write function to compute Pascal's triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -8,3 +8,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
+    # create list triangle
+    tri = [[1 for i in range(j + 1)] for j in range(n)]
+
+    for row_n, row in enumerate(tri):
+        for col in range(len(row) - 1):
+            if col is 0:  # skip [n][0] index
+                continue
+            tri[row_n][col] = tri[row_n - 1][col - 1] + tri[row_n - 1][col]
+    return tri
