9c2b29dc636f2b75db341ab75e69c7694fd7a7b5
justinmajetich <justinmajetich@gmail.com>
2020-01-23 14:01:32 +0000

    Remove while loop

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,18 +7,22 @@ codes = ['200', '301', '400', '401',
          '403', '404', '405', '500']
 code_hist = []
 total_size = 0
+i = 0
 try:
-    while True:
-        for cnt, line in enumerate(sys.stdin):
-            tokens = line.split(' ')
-            total_size += int(tokens[-1])
-            code_hist.append(tokens[-2])
-            if cnt == 9:
-                print('File size: ' + str(total_size))
-                for code in codes:
-                    if code in code_hist:
-                        print('{}: {}'.format(code, code_hist.count(code)))
-                break
+    for line in sys.stdin:
+        # if line == '':
+        # sys.exit()
+        tokens = line.split(' ')
+        total_size += int(tokens[-1])
+        code_hist.append(tokens[-2])
+        if i == 9:
+            i = 0
+            print('File size: ' + str(total_size))
+            for code in codes:
+                if code in code_hist:
+                    print('{}: {}'.format(code, code_hist.count(code)))
+        else:
+            i += 1
 except KeyboardInterrupt:
     print('File size: ' + str(total_size))
     for code in codes:
