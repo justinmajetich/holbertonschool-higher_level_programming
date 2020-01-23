1d2954a2929f0d909c67fa859fc4cc14cd01fe19
justinmajetich <justinmajetich@gmail.com>
2020-01-23 00:07:18 +0000

    Remove docstrings

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a program that parses stdin
-
-The line is taken from stdin and split into distinct tokens. The
-tokens or interest are added to a sequence and counted on a need
-to know basis.
-"""
-
 import sys
 codes = ['200', '301', '400', '401',
          '403', '404', '405', '500']
 code_hist = []
 total_size = 0
-"""These variables are used to track codes received and filesize
-"""
 try:
     while True:
         for cnt, line in enumerate(sys.stdin):
