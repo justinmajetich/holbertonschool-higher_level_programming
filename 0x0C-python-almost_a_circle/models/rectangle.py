007ca35bbe433ef094ddaaf5a0feb8f55574ec4b
justinmajetich <justinmajetich@gmail.com>
2020-01-23 19:48:06 +0000

    Implement validators in setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,6 +35,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set value of private variable width"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -45,6 +49,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set value of private variable height"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -55,6 +63,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set value of private variable x"""
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -65,4 +77,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set value of private variable y"""
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
