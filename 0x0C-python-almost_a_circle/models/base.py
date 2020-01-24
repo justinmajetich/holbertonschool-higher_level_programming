fa6faf833356968cd2b239fc0d3d19cabe064020
justinmajetich <justinmajetich@gmail.com>
2020-01-24 18:16:51 +0000

    Implement save_to_file class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,6 +27,30 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write JSON representation to file.
+
+        The method first converts the objects passed
+        into dictionary representations of themselves
+        using the to_dictionary method included in all
+        Base subclasses. These dictionaries are stored
+        in a temp list, converted to JSON strings and
+        returned.
+
+        Arguments
+            list_objs: list of subclass instances
+        """
+        if not list_objs:  # if empty/None, save empty list
+            list_objs = []
+
+        dict_list = []
+        for obj in list_objs:  # convert objs to dicts
+            dict_list.append(obj.to_dictionary())
+
+        with open((cls.__name__ + '.json'), mode='w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(dict_list))
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return JSON string representation or argument
@@ -34,6 +58,6 @@ class Base:
         Arguments
             list_dictionaries: a list of dictionaries
         """
-        if not list_dictionaries:
+        if not list_dictionaries or type(list_dictionaries) is not list:
             return '[]'
         return json.dumps(list_dictionaries)
