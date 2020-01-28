9327af070a0c56b3c749ab727ca70d63d49c108a
justinmajetich <justinmajetich@gmail.com>
2020-01-28 01:58:07 +0000

    Complete test cases for Base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,7 +4,7 @@
 
 import unittest
 import json
-from os import path
+from os import path, remove
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -91,7 +91,7 @@ class TestBaseJSONToString(unittest.TestCase):
         self.assertEqual(self.obj.to_json_string(666), '666')
 
     def test_empty_list_of_dict_arg(self):
-        """Converts list of empty dicts to JSON string""" 
+        """Converts list of empty dicts to JSON string"""
         self.assertEqual(self.obj.to_json_string([{}, {}]), '[{}, {}]')
 
     def test_none(self):
@@ -107,6 +107,7 @@ class TestBaseJSONToString(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.obj.to_json_string()
 
+
 class TestBaseJSONToFile(unittest.TestCase):
     """This class contains test for Base's save_to_file method"""
 
@@ -116,9 +117,6 @@ class TestBaseJSONToFile(unittest.TestCase):
         self.r1 = Rectangle(4, 6)
         self.s0 = Square(9)
         self.s1 = Square(3, 1, 1)
-
-    def tearDown(self):
-        """Wipe test state"""
 
     def test_file_created(self):
         """File is created"""
@@ -137,8 +135,10 @@ class TestBaseJSONToFile(unittest.TestCase):
             read = f.read()
         self.assertEqual(read, '[]')
 
-    def test_list_of_one_file(self):
-        """ """
+    def test_list_of_one_obj(self):
+        """Saves a simple obj representation to file"""
+        with self.assertRaises(TypeError):
+            self.s1.save_to_file(self.r1)
 
     def test_list_of_non_subclass_objs(self):
         """Saving non-subclass objects raises AttributeError"""
@@ -149,3 +149,117 @@ class TestBaseJSONToFile(unittest.TestCase):
         """Missing argument raises TypeError"""
         with self.assertRaises(TypeError):
             self.r1.save_to_file()
+
+
+class TestBaseJSONToDict(unittest.TestCase):
+    """This class contains test for Base's from_json_string method"""
+
+    def setUp(self):
+        """Build test state"""
+        self.d = [{'id': 99, 'name': 'joe'}, {'date': '042891'}]
+        self.r0 = Rectangle(2, 3, 0, 0)
+        self.s0 = Square(3, 1, 1)
+        self.string = self.r0.to_json_string(self.d)
+
+    def test_return_value(self):
+        """Method returns correct values"""
+        self.assertEqual(self.r0.from_json_string(self.string), self.d)
+
+    def test_return_type(self):
+        """Method returns correct type"""
+        self.assertEqual(type(self.s0.from_json_string(self.string)), list)
+
+    def test_non_list_arg(self):
+        """Raises TypeError"""
+        with self.assertRaises(TypeError):
+            self.r0.from_json_string(666)
+
+    def test_list_of_non_dicts(self):
+        """"""
+        with self.assertRaises(ValueError):
+            self.s0.from_json_string('[66, \'66\']')
+
+    def test_none_arg(self):
+        """Returns empty list"""
+        self.assertEqual(self.r0.from_json_string(None), [])
+
+    def test_missing_arg(self):
+        """Raises TypeError"""
+        with self.assertRaises(TypeError):
+            self.r0.from_json_string()
+
+
+class TestBaseJSONFromFile(unittest.TestCase):
+    """This class contains test for Base's load_from_file method"""
+
+    def setUp(self):
+        """Build test state"""
+        Base._Base__nb_objects = 0
+        self.r0 = Rectangle(2, 3, 0, 0)
+        self.r1 = Rectangle(4, 6)
+        self.s0 = Square(9)
+        self.s1 = Square(3, 1, 1)
+
+    def test_good_load(self):
+        """Loads list of instances from file"""
+        self.r0.save_to_file([self.r0, self.r1])
+        objs = self.r0.load_from_file()
+        self.assertEqual(str(objs[0]), '[Rectangle] (1) 0/0 - 2/3')
+        self.assertEqual(str(objs[1]), '[Rectangle] (2) 0/0 - 4/6')
+
+    def test_non_existent_file(self):
+        """Returns an empty list"""
+        remove('Rectangle.json')
+        self.assertEqual(self.r1.load_from_file(), [])
+
+    def test_type_of_loaded_sq_objs(self):
+        """Loads file data into Square(s)"""
+        self.s0.save_to_file([self.r0])
+        objs = self.s1.load_from_file()
+        self.assertEqual(type(objs[0]), Square)
+
+    def test_type_of_loaded_rect_objs(self):
+        """Loads file data into Rectangle(s)"""
+        self.r0.save_to_file([self.s0, self.r1])
+        objs = self.r1.load_from_file()
+        self.assertEqual(type(objs[1]), Rectangle)
+
+    def test_empty_file(self):
+        """Empty file returns empty list"""
+        with open('Rectangle.json', mode='w', encoding='utf-8') as f:
+            f.write('')
+        self.assertEqual(self.r1.load_from_file(), [])
+
+
+class TestBaseCreate(unittest.TestCase):
+    """This class contains tests for Base's create method"""
+
+    def setUp(self):
+        """Build test state"""
+        Base._Base__nb_objects = 0
+        self.r0 = Rectangle(2, 3, 0, 0)
+        self.r1 = Rectangle(4, 6)
+        self.s0 = Square(9)
+        self.s1 = Square(3, 1, 1)
+
+    def test_create_rectangle(self):
+        """Returns initialized Rectangle instance"""
+        d = self.r1.to_dictionary()
+        r3 = Rectangle.create(**d)
+        self.assertEqual(str(r3), str(self.r1))
+
+    def test_create_square(self):
+        """Returns initialized Square instance"""
+        d = self.s1.to_dictionary()
+        s3 = Square.create(**d)
+        self.assertEqual(str(s3), str(self.s1))
+
+    def test_missing_args(self):
+        """Creates instance with dummy attributes"""
+        s = Square.create()
+        self.assertEqual(str(s), '[Square] (5) 0/0 - 1')
+
+    def test_invalid_arg(self):
+        """Raises TypeError"""
+        with self.assertRaises(TypeError):
+            s = Square.create(666)
