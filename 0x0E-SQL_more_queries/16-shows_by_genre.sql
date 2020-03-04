6394d3cb7ad33b8a252c35ae860dc045049cd64c
justinmajetich <justinmajetich@gmail.com>
2020-03-04 01:02:44 +0000

    Add second ORDER BY case

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ SELECT tv_shows.title, tv_genres.name
 	ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres
 	ON tv_genres.id = tv_show_genres.genre_id
-	ORDER BY tv_shows.title ASC;
+	ORDER BY tv_shows.title ASC, tv_genres.name ASC;
