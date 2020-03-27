7a862b14f08a92388a687fcdb9f0edfd88f82940
justinmajetich <justinmajetich@gmail.com>
2020-03-27 23:49:54 +0000

    Write python script to query MySQL db

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Select states starting with N from database """
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
+
+    cursor = database.cursor()
+
+    cursor.execute('SELECT id, name FROM states\
+                   WHERE SUBSTRING(name, 1, 1) = \'N\'\
+                   ORDER BY states.id ASC')
+
+    for row in cursor.fetchall():
+        print(row)
