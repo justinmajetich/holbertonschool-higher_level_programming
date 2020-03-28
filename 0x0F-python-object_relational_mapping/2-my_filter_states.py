f6206ae3001f6b549b71d71f3a032c92b5dda89e
justinmajetich <justinmajetich@gmail.com>
2020-03-28 17:03:15 +0000

    Revert to previous method

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     cursor = database.cursor()
 
     cursor.execute('SELECT id, name FROM states\
-                   ORDER BY states.id ASC')
+                   WHERE name = \'{}\'\
+                   ORDER BY states.id ASC'.format(search))
 
     for row in cursor.fetchall():
-        if search in row:
-            print(row)
+        print(row)
