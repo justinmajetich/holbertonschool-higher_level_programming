9913b4d21e9884142e1fa50ce59ada6efbc75b96
justinmajetich <justinmajetich@gmail.com>
2020-03-28 16:21:33 +0000

    Write query to fetch first row with SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" List all state objects using sqlalchemy """
+
+if __name__ == '__main__':
+
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm.session import sessionmaker, Session
+    from model_state import Base, State
+
+    username = '{}'.format(argv[1])
+    password = '{}'.format(argv[2])
+    db_name = '{}'.format(argv[3])
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).order_by(State.id).first()
+    if result:
+        print('{}: {}'.format(result.id, result.name))
+    else:
+        print('Nothing')
