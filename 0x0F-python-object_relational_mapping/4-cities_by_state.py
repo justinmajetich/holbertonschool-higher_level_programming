45ac410bf733d6ac2b629ac56d7e881aea983878
justinmajetich <justinmajetich@gmail.com>
2020-03-28 20:00:23 +0000

    Sort by cities.id not states.id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     cursor.execute('SELECT cities.id, cities.name, states.name FROM cities\
                    JOIN states\
                    ON cities.state_id = states.id\
-                   ORDER BY states.id ASC')
+                   ORDER BY cities.id ASC')
 
     for row in cursor.fetchall():
         print(row)
