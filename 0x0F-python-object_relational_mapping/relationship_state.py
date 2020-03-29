cd32c946f9afd19cf04fcef34d0ad5dab739d00d
justinmajetich <justinmajetich@gmail.com>
2020-03-29 16:51:58 +0000

    Add state and city relationship models

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Define State model """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class State(Base):
+    """ Define a class State to be linked to db table """
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False,
+                autoincrement=True, unique=True,
+                primary_key=True)
+
+    name = Column(String(128), nullable=False)
+
+    cities = relationship('City', cascade='all, delete', backref='states')
