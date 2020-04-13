a7d5249f3f7622a36f85c203c25667a6b2795e75
justinmajetich <justinmajetich@gmail.com>
2020-04-13 17:53:24 +0000

    Write script to parse JSON response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Make a request with parameters
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if len(argv) > 1:
+        q = argv
+    else:
+        q = ''
+
+    r = requests.post(url, data={'q': q})
+
+    try:
+        r = r.json()
+
+        if not r:
+            print('No result')
+        else:
+            print('[{}] {}'.format(r.get('id'), r.get('name')))
+
+    except ValueError:
+        print('Not a valid JSON')
