5abf21853bc297e2a1a1117eaad0f091610a1c95
justinmajetich <justinmajetich@gmail.com>
2020-04-13 16:08:07 +0000

    Include list header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-
+    print('Body response:')
     print('\t- type: {}'.format(type(content)))
     print('\t- content: {}'.format(content))
     print('\t- utf8 content: {}'.format(str(content)[2:-1]))
