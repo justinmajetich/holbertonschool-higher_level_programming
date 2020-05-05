ca7b45e78d8aa5dbb88877dc8953d7e065ed8dcb
justinmajetich <justinmajetich@gmail.com>
2020-05-05 18:37:07 +0000

    Return second largest int in sequence instead of largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,9 +6,8 @@ let argv = process.argv;
 if (argv.length < 4) { console.log(0); } else {
   // Strip extraneous args
   argv = argv.slice(2);
-  // Convert elems to ints
-  const nums = argv.map(x => parseInt(x));
+  // Convert elems to ints and sort
+  const nums = argv.map(x => parseInt(x)).sort();
   // Find max int in list
-  const max = Math.max(...nums);
-  console.log(max);
+  console.log(nums[nums.length - 2]);
 }
