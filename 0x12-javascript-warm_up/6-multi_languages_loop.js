98a4bf6d0e59b6cf229faa482115a80a9b94341f
justinmajetich <justinmajetich@gmail.com>
2020-05-05 16:43:19 +0000

    Implement while loop in JS

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Basic print with Javascript
+
+const array = ['C is fun',
+  'Python is cool',
+  'Javascript is amazing'];
+let i = 0;
+
+while (array[i]) {
+  console.log(array[i]);
+  i++;
+}
