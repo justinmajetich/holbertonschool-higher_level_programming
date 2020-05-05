7f446599927bb1d3922e3c9d0d2dea0e082c99fc
justinmajetich <justinmajetich@gmail.com>
2020-05-05 18:53:58 +0000

    Specify function for sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,9 @@ let argv = process.argv;
 if (argv.length < 4) { console.log(0); } else {
   // Strip extraneous args
   argv = argv.slice(2);
-  // Convert elems to ints and sort
-  const nums = argv.map(x => parseInt(x)).sort();
+  // Convert elems to ints
+  let nums = argv.map(x => parseInt(x));
+  nums = nums.sort(function (a, b) { return a - b; });
   // Find max int in list
   console.log(nums[nums.length - 2]);
 }
