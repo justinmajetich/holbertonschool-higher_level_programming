8a1568bb9c780de798ff1bc0aa7b725e2e49ef4d
justinmajetich <justinmajetich@gmail.com>
2020-05-11 20:11:49 +0000

    Add space to print line

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.logMe = function (item) {
   if (this.count === undefined) { this.count = 0; } else { this.count++; }
-  const line = this.count + ':' + item;
+  const line = this.count + ': ' + item;
   console.log(line);
 };
