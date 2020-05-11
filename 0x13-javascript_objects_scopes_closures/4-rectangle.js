5431d8c6210375e0d18ecf64a3d8bf0aeee49b3c
justinmajetich <justinmajetich@gmail.com>
2020-05-11 18:52:11 +0000

    Add double and rotate methods to class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const line = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
