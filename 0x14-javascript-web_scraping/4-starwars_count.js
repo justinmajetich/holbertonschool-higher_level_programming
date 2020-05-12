f81843f0ed5aa34e6c0a80ffd9d0441179e66348
justinmajetich <justinmajetich@gmail.com>
2020-05-12 22:02:23 +0000

    Add optional / to url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+const wedge = 'https://swapi-api.hbtn.io/api/people/18';
 
 request(url, (error, response, body) => {
   if (error) { console.log(error); }
   const jsonBody = JSON.parse(body);
   let wedgeCount = 0;
   for (const result of jsonBody.results) {
-    if (result.characters.includes(wedge)) {
+    const charList = result.characters;
+    if (charList.includes(wedge) || charList.includes(wedge + '/')) {
       wedgeCount++;
     }
   }
