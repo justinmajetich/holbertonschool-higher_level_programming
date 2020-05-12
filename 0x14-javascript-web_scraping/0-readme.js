e160feb58d0b8eca06a0d98e752e1be1ca46f166
justinmajetich <justinmajetich@gmail.com>
2020-05-12 14:26:41 +0000

    Write script to read from file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2]
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error)
+    console.log(error);
+  else
+    console.log(data);
+});
