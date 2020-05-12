8ae28bb39d444a253e5956972362c58de556189a
justinmajetich <justinmajetich@gmail.com>
2020-05-12 18:34:35 +0000

    Write request with await keyword

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,14 +3,14 @@ const request = require('request');
 const movieID = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-request(url, (error, response, body) => {
+request(url, async (error, response, body) => {
   if (error) { console.log(error); }
   const result = JSON.parse(body);
+
   for (const charURL of result.characters) {
-    request(charURL, (err, r, body) => {
+    await request(charURL, (err, r, body) => {
       if (err) { console.log(err); }
-      const result = JSON.parse(body);
-      console.log(result.name);
+      console.log(JSON.parse(body).name);
     });
   }
 });
